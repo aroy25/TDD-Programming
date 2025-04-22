f2dd729c5447e092ec1a8c8784a432beebf9efc7
matty-michel <mmichel@carthage.edu>
2025-04-22 13:56:15 -0500

    added point class

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <assert.h>
+#include "point.h"
 
 using namespace std;
 int main()
 {
+    point newPoint(1.5, 2.5);
+    
+    newPoint.toString(1.5, 2.5);
     cout << "all test pass" << endl;
     return 0;
 }
